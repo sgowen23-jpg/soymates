be5f3b30e9ef134bc7cbd8e0ca46fe59d9dfb1ed
sgowen23-jpg <sgowen23@gmail.com>
2026-03-22 12:15:23 +1030

    Remove client filter, move search to first position on map toolbar

--- src/pages/StoreMap/index.jsx
+++ src/pages/StoreMap/index.jsx
@@ -4,37 +4,32 @@ import StoreProfile from './StoreProfile'
 import StoreSearchInput from '../../components/StoreSearchInput'
 import './StoreMap.css'
 
-const CLIENTS = ['All', 'Vitasoy']
-const STATES  = ['All', 'NSW', 'QLD', 'SA', 'VIC', 'WA']
-const REPS    = ['All', 'Azra Horell', 'Ashleigh Tasdarian', 'David Kerr', 'Dipen Surani', 'Sam Gowen', 'Shane Vandewardt']
+const STATES = ['All', 'NSW', 'QLD', 'SA', 'VIC', 'WA']
+const REPS   = ['All', 'Azra Horell', 'Ashleigh Tasdarian', 'David Kerr', 'Dipen Surani', 'Sam Gowen', 'Shane Vandewardt']
 
 export default function StoreMap() {
   const [selectedStore, setSelectedStore] = useState(null)
-  const [client, setClient] = useState('All')
   const [state, setState] = useState('All')
   const [rep, setRep] = useState('All')
   const [search, setSearch] = useState('')
 
-  const filters = { client, state, rep }
+  const filters = { state, rep }
 
   return (
     <div className="storemap-page">
       <div className="storemap-toolbar">
         <div className="filter-dropdowns">
-          <select className="filter-select" value={client} onChange={e => setClient(e.target.value)}>
-            {CLIENTS.map(c => <option key={c}>{c}</option>)}
-          </select>
+          <StoreSearchInput
+            value={search}
+            onChange={setSearch}
+            placeholder="Search store…"
+          />
           <select className="filter-select" value={state} onChange={e => setState(e.target.value)}>
             {STATES.map(s => <option key={s}>{s}</option>)}
           </select>
           <select className="filter-select" value={rep} onChange={e => setRep(e.target.value)}>
             {REPS.map(r => <option key={r}>{r}</option>)}
           </select>
-          <StoreSearchInput
-            value={search}
-            onChange={setSearch}
-            placeholder="Search store…"
-          />
         </div>
 
         <span className="storemap-title">Store Map</span>
